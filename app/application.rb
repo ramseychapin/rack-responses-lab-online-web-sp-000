02c2fc5576fc1c8d11591cabe8d4437e1bbb24fe
Ramsey Chapin <ramsey@ramseychapin.com>
2020-02-01 23:14:46 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp=Rack::Response.new
 
-    if Time.now.hour.before?(0,11)
+    if Time.now.hour.before?(11)
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
